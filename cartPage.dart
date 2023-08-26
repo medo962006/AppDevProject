a783b8869b2a2b04406d23bfca074606a4dd2a2c
medo962006 <63151191+medo962006@users.noreply.github.com>
2023-08-26 16:39:30 +0300

    Add files via upload

---
+++ cartPage.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class CartPage extends StatelessWidget {
+  List<String> cart = ['', '', '', '', '', '', '', ''];
+  List<bool> cartBool = [];
+  List<String> itemsNames = [
+    'Lettuce',
+    'Tomato',
+    'Cucumber',
+    'Milk',
+    'Eggs',
+    'Rice',
+    'Sugar',
+    'Bread',
+    'Cheese'
+  ];
+  List<Widget> funct(int index) {
+    List<Widget> _Widgets = [];
+    if (cart.length != 0) {
+      if (cart[index] != "") {
+        var x = itemsNames[index];
+        _Widgets.add(Image.asset("images/$x.jpg"));
+        _Widgets.add(Text(x));
+        _Widgets.add(Container(
+          width: 200,
+          alignment: Alignment.centerRight,
+          child: ElevatedButton(
+              onPressed: () => {cart[index] = ""},
+              child: Text("remove from cart")),
+        ));
+      }
+    }
+    return _Widgets;
+  }
+
+  String currentText = "";
+  CartPage({required this.cart, required this.cartBool});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(children: [
+      ElevatedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.arrow_back_ios_new),
+      ),
+      if (cartBool[0] == true) ...[
+        SizedBox(
+          width: 510,
+          height: 50,
+          child: Row(
+            children: funct(0),
+          ),
+        )
+      ],
+      if (cartBool[1] == true) ...[
+        SizedBox(width: 510, height: 50, child: Row(children: funct(1))),
+        if (cartBool[2] == true) ...[
+          SizedBox(
+            width: 510,
+            height: 50,
+            child: Row(children: funct(2)),
+          )
+        ],
+        if (cartBool[3] == true) ...[
+          SizedBox(
+            width: 510,
+            height: 50,
+            child: Row(children: funct(3)),
+          )
+        ],
+        if (cartBool[4] == true) ...[
+          SizedBox(
+            width: 510,
+            height: 50,
+            child: Row(children: funct(4)),
+          )
+        ],
+        if (cartBool[5] == true) ...[
+          SizedBox(
+            width: 510,
+            height: 50,
+            child: Row(children: funct(5)),
+          )
+        ],
+        if (cartBool[6] == true) ...[
+          SizedBox(
+            width: 510,
+            height: 50,
+            child: Row(children: funct(6)),
+          )
+        ],
+        if (cartBool[7] == true) ...[
+          SizedBox(
+            width: 510,
+            height: 50,
+            child: Row(children: funct(7)),
+          )
+        ],
+        if (cartBool[8] == true) ...[
+          SizedBox(
+            width: 510,
+            height: 50,
+            child: Row(children: funct(8)),
+          )
+        ]
+      ],
+    ]));
+  }
+}
